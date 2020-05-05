fd97a8f3f3168f92fbf44dbd4a76a7d53656b7c1
Tony Pham <tony.phamx04@gmail.com>
2020-05-04 20:00:22 -0400

    Update: Remove 'Themes' from menu bar

--- app.py
+++ app.py
@@ -26,16 +26,3 @@ class App:
         file.add_command(label='Add Task', command=lambda: self.content.add_task())
         file.entryconfig('Add Task', accelerator='CTRL+N')
         self.menubar.add_cascade(menu=file, label='File')
-
-        # THEMES MENU
-        themes = Menu(self.menubar)
-        gimp_themes = Menu(themes)
-        gimp_themes.add_command(label='Maple Leaves (Default)', command=lambda: self.change_theme('images/gimp_themes/maple_leaves.png'))
-        gimp_themes.add_cascade(label='Lightning', command=lambda: self.change_theme('images/gimp_themes/lightning.png'))
-        gimp_themes.add_cascade(label='Chocolate Swirl', command=lambda: self.change_theme('images/gimp_themes/chocolate_swirl.png'))
-        themes.add_cascade(menu=gimp_themes, label = 'GIMP Themes')
-        self.menubar.add_cascade(menu=themes, label='Themes')
-
-    def change_theme(self, image_path=None, foreground=None, background=None):
-        if image_path is not None:
-            self.banner.change_image(image_path)
